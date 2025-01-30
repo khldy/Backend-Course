5f9a293076e8d85894bc756b406c5679f12844d7
khldy <20212022103@std.nisantasi.edu.tr>
2025-01-30 23:53:14 +0300

    Section 1 - Done

--- index.js
+++ index.js
@@ -1,38 +1,95 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+const { strict } = require('assert');
 
 //////////////////////////////////
 // FILES
-//Blockking, synchronous way
-// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8'); 
-// console.log(textIn);
-// const textOut = `This is what we know about the avcado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./starter/txt/output.txt', textOut);
-// console.log('File written!');
 
-// //Non-blockking, asynchronous way
-// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-//     if (err) return console.log('ERROR! 💥');
-//     fs.readFile(`./starter/txt/${data1}.txt', 'utf-8`, (err, data2) => {
-//         console.log(data2);
-//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-//             console.log(data3);
+// Read JSON data
+const data = fs.readFileSync(`starter/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
-//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}` , err => {
-//                 console.log('Your file has been written! 🎉');
-//             })
-//         });
-//     });
-// });
-// console.log('Will read file!');
+// Read HTML templates
+const tempOverview = fs.readFileSync(
+  `${__dirname}/starter/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/starter/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/starter/templates/template-product.html`,
+  'utf-8'
+);
 
 //////////////////////////////////
 // SERVER
 
+const slugs = dataObj.map((el) => ({
+  ...el,
+  slug: slugify(el.productName, {
+    lower: true,
+    strict: true,
+  }),
+}));
+console.log(slugs);
+
+// const toSlug (str) => slugify(str, { lower: true, strict: true });
+// const upadtedData = dataObj.map(el => {
+//   return {
+//     ...el,
+//     id: toSlug(el.productName)
+//   };
+// });
+
+console.log(slugify('Fresh Avocadoes', { lowerCase: true })); // fresh-avocadoes
+
 const server = http.createServer((req, res) => {
-    res.end('Hello from the sever!');
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%', cardsHTML);
+
+    res.end(output);
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+    const product = dataObj[query.id]; // Replace with dynamic product data based on the request
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'content-type': 'application/json',
+    });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      'content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found</h1>');
+  }
 });
 
-server.listen(8000, '127.0.0.1' , () => {
-    console.log('Listening to requests on port 8000');
-})
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
